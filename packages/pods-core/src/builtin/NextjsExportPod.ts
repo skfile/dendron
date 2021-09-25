ab257c8e4be31272ff0a16bb5e7c78689e31c879
Kevin <kevin@dendron.so>
2021-09-24 18:21:48 -0700

    spike: work for putting note-ref into iframe

--- packages/pods-core/src/builtin/NextjsExportPod.ts
+++ packages/pods-core/src/builtin/NextjsExportPod.ts
@@ -1,13 +1,21 @@
 import {
+  createSerializedFuseNoteIndex,
   DendronConfig,
   DendronSiteConfig,
   DEngineClient,
   NoteProps,
   NotePropsDict,
   NoteUtils,
-  createSerializedFuseNoteIndex,
 } from "@dendronhq/common-all";
-import { MDUtilsV5, ProcFlavor, SiteUtils } from "@dendronhq/engine-server";
+import {
+  DendronASTDest,
+  getRefId,
+  MDUtilsV5,
+  ProcFlavor,
+  SerializedNoteRef,
+  SiteUtils,
+  UnistNode,
+} from "@dendronhq/engine-server";
 import { JSONSchemaType } from "ajv";
 import fs from "fs-extra";
 import _ from "lodash";
@@ -98,6 +106,43 @@ export class NextjsExportPod extends ExportPod<NextjsExportConfig> {
     );
     const payload = await proc.process(NoteUtils.serialize(note));
     return payload.toString();
+  }
+
+  async _renderRef({
+    engine,
+    note,
+    notes,
+    engineConfig,
+    node,
+  }: {
+    engine: DEngineClient;
+    note: NoteProps;
+    notes: NotePropsDict;
+    node: UnistNode;
+    engineConfig: DendronConfig;
+  }) {
+    const proc = MDUtilsV5.procRemarkFull({
+      dest: DendronASTDest.MD_DENDRON,
+      engine,
+      fname: note.fname,
+      vault: note.vault,
+      config: engineConfig,
+      notes,
+    });
+    const out = proc.stringify(node);
+    const proc2 = MDUtilsV5.procRehypeFull(
+      {
+        engine,
+        fname: note.fname,
+        vault: note.vault,
+        config: engineConfig,
+        notes,
+      },
+      { flavor: ProcFlavor.REGULAR }
+    );
+    const resp = await proc2.process(out);
+    // TODO: add custom iframe code here
+    return resp.contents;
   }
 
   _writeEnvFile({
@@ -276,6 +321,8 @@ export class NextjsExportPod extends ExportPod<NextjsExportConfig> {
     // render notes
     const notesBodyDir = path.join(podDstDir, "notes");
     const notesMetaDir = path.join(podDstDir, "meta");
+    const notesRefsDir = path.join(podDstDir, "refs");
+
     this.L.info({ ctx, msg: "ensuring notesDir...", notesDir: notesBodyDir });
     fs.ensureDirSync(notesBodyDir);
     fs.ensureDirSync(notesMetaDir);
@@ -295,7 +342,34 @@ export class NextjsExportPod extends ExportPod<NextjsExportConfig> {
         ]);
       })
     );
+
+    // render refs
+    const refsRoot = MDUtilsV5.getRefsRoot(wsRoot);
+    // doesn't matter what the note is, this is why we just take first note from list
+    const noteForRef = _.values(publishedNotes)[0];
+    const refIds: string[] = await Promise.all(
+      fs.readdirSync(refsRoot).map(async (ent) => {
+        const { node, refId } = fs.readJSONSync(
+          path.join(refsRoot, ent)
+        ) as SerializedNoteRef;
+        const out = await this._renderRef({
+          engine,
+          note: noteForRef,
+          node,
+          engineConfig,
+          notes: publishedNotes,
+        });
+        const refIdString = getRefId(refId);
+        const dst = path.join(notesRefsDir, refIdString + ".html");
+        this.L.debug({ ctx, dst, msg: "writeNote" });
+        fs.ensureFileSync(dst);
+        fs.writeFileSync(dst, out);
+        return refIdString;
+      })
+    );
+
     const podDstPath = path.join(podDstDir, "notes.json");
+    const refDstPath = path.join(podDstDir, "refs.json");
     const podConfigDstPath = path.join(podDstDir, "dendron.json");
     fs.writeJSONSync(
       podDstPath,
@@ -306,6 +380,10 @@ export class NextjsExportPod extends ExportPod<NextjsExportConfig> {
       { encoding: "utf8", spaces: 2 }
     );
     fs.writeJSONSync(podConfigDstPath, engineConfig, {
+      encoding: "utf8",
+      spaces: 2,
+    });
+    fs.writeJSONSync(refDstPath, refIds, {
       encoding: "utf8",
       spaces: 2,
     });
